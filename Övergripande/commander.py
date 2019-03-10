417a19afe0c7134ab87d26421b62f3054daeb462
christka1 <44722191+christka1@users.noreply.github.com>
2019-03-10 15:23:47 +0100

    Commander can now handle multiple products in multiple stations

--- Övergripande/commander.py
+++ Övergripande/commander.py
@@ -14,88 +14,145 @@ import time
 from commander_msgs.msg import Command
 from commander_msgs.msg import State
 
+#Defining pubs and subs
+pubs = []
+subs = []
 
 #Defining products, a station array and work order array
-no_product = "no_product"
 product_a = "product_a"    #Assuming 2 different products
 product_b = "product_b"
-product_in_station = [no_product, no_product, no_product]  #Assuming 3 stations
+product_in_station = []                                                                             #Is this necessary?
 work_order = []      #First-in-first-out order, gets popped when going to the first station
 
-#Defining messages and organizing them in arrays
-cmd_msgs = [Command(), Command(), Command()]
-state_msgs = [State(), State(), State()]
+#Defining message arrays
+cmd_msgs = []
+state_msgs = []
 
+node = None
+
+sleep_time = 1
 
 def main(args=None):
-    #Initializing node, pub and sub
-    rclpy.init(args=args)
-    node = rclpy.create_node('command_node')
-    pub = node.create_publisher(Command, 'cmd1')
-    sub = node.create_subscription(State, 'state1', state1_callback)
-    #Done with node, pub and sub
+    global node
 
+    noOfStations = 3
+    node = init(noOfStations)
 
     create_work_order()     #Puts products in work_order in a predefined pattern
 
-    while not(product_in_station[0] == no_product and product_in_station[1] == no_product and product_in_station[2] == no_product) or len(work_order) != 0:
-        station = 2     #Last station
-        while station >= 0:
-            print("Kollar station " + str(station))
-            time.sleep(1)
-            if product_in_station[station] != no_product:
-                rclpy.spin_once(node)
-                print("Kolla om station " + str(station) + " är klar")
-                time.sleep(4)
-                if state_msgs[station].finished:
-                    product_in_station[station+1] = product_in_station[station]
-                    product_in_station[station] = no_product
-                    cmd_msgs[station].run = False
-                    while not state_msgs[station].init:
-                        pub.publish(cmd_msgs[station])
-                        rclpy.spin_once(node)
-                        print("Pubbar run=false tills jag får init=true")
-                        time.sleep(4)
+    while not(product_in_station[0] == None and product_in_station[1] == None and product_in_station[2] == None) or len(work_order) != 0:
 
-                        #Kan bara sätta en sak som klar om den kan skickas vidare, så kolla stationen framför.
+        station = noOfStations - 1     #Last station
+
+        while station >= 0:
+
+            print("Checking station " + str(station))
+            time.sleep(sleep_time)
+            rclpy.spin_once(node)
+
+            if product_in_station[station] != None:
+
+                print("Checking if station " + str(station) + " is done")
+                time.sleep(sleep_time)
+
+                if state_msgs[station].finished:
+                    if station == noOfStations - 1:     #Last station => no station to send forward to
+                        station_done(station)
+
+                    elif state_msgs[station+1].init:    #Not last station => check forward
+                        product_in_station[station+1] = product_in_station[station]
+                        station_done(station)           #Should a station be done before the next station has started on the product?
+                        send_command(station+1)
+
+                    else:
+                        print("Station " + str(station) + " is done but the station ahead is not ready")
+
+                else:
+                    print("Station " + str(station) + " is not done")
+
             else:
                 if station == 0 and len(work_order) != 0:
-                    #Abstrahera till egen metod? Är ju bara för station 0 ändå
-                    #Går också att abstrahera hela proceduren med att skicka meddelande, vänta på handskakning och sedan sätta run till true
-                    print("Skicka in nästa order till station " + str(station))
-                    product_in_station[station] = work_order.pop(0)
-                    cmd_msgs[station].command = "Assemble in station " + str(station)
-                    cmd_msgs[station].run = False
-                    cmd_msgs[station].product_name = product_in_station[station]
-                    while state_msgs[station].cmd != cmd_msgs[station].command:
-                        pub.publish(cmd_msgs[station])
-                        print("Väntar på svar från station " + str(station) + " med meddelandet " + cmd_msgs[station].command)
-                        rclpy.spin_once(node)
-                        time.sleep(4)
-                    cmd_msgs[station].run = True
-                    while not state_msgs[station].executing:
-                        pub.publish(cmd_msgs[station])
-                        rclpy.spin_once(node)
-                        print("Pubbar run=true tills jag får executing=true")
-                        time.sleep(4)
-                else:
-                    print("Inget att göra")
-                    time.sleep(1)
+                    if not state_msgs[station].init:
 
-            station = station - 1
+                        print("Station " + str(station) + " is not ready for another work order")
+
+                    else:
+                        print("Send in the next order to station " + str(station))
+                        product_in_station[station] = work_order.pop(0)
+                        send_command(station)
+                else:
+                    print("Nothing to do")
+                    time.sleep(sleep_time)
+
+            station -= 1
+
     print(product_in_station)
     print(work_order)
 
     node.destroy_node()
     rclpy.shutdown()
 
-def state1_callback(state_msg):
+def init(j):
+    rclpy.init()
+    node = rclpy.create_node('command_node')
+
+    #pubs.append(node.create_publisher(Command, 'cmd0'))
+    #pubs.append(node.create_publisher(Command, 'cmd1'))
+    #pubs.append(node.create_publisher(Command, 'cmd2'))
+    subs.append(node.create_subscription(State, 'state0', state0_callback))
+    subs.append(node.create_subscription(State, 'state1', state1_callback))     #Would be nice to make a general solution with subs instead of hardcoding
+    subs.append(node.create_subscription(State, 'state2', state2_callback))     #But don't know how to make the methods in a good way
+
+    i = 0
+    while j > i:
+        pubs.append(node.create_publisher(Command, 'cmd' + str(i)))
+
+        product_in_station.append(None)
+
+        cmd_msgs.append(Command())
+        state_msgs.append(State())
+        i += 1
+
+    return node
+
+def state0_callback(state_msg):
     state_msgs[0] = state_msg
+
+def state1_callback(state_msg):
+    state_msgs[1] = state_msg
+
+def state2_callback(state_msg):
+    state_msgs[2] = state_msg
 
 def create_work_order():
     work_order.append(product_a)
-    #work_order.append(product_b)
+    work_order.append(product_b)
     #work_order.append(product_a)
+
+def station_done(station):
+    product_in_station[station] = None
+    cmd_msgs[station].run = False
+    while not state_msgs[station].init:
+        pubs[station].publish(cmd_msgs[station])
+        rclpy.spin_once(node)
+        print("Pubing run=false until init=true on station " + str(station))
+        time.sleep(sleep_time)
+
+def send_command(station):
+    cmd_msgs[station].command = "Assemble in station " + str(station)
+    cmd_msgs[station].run = False
+    cmd_msgs[station].product_name = product_in_station[station]
+    while state_msgs[station].cmd != cmd_msgs[station].command:
+        pubs[station].publish(cmd_msgs[station])
+        print("Waiting on station " + str(station) + " with the message " + cmd_msgs[station].command)
+        rclpy.spin_once(node)
+        time.sleep(sleep_time)
+    cmd_msgs[station].run = True
+    while not state_msgs[station].executing:
+        pubs[station].publish(cmd_msgs[station])
+        rclpy.spin_once(node)
+        print("Pubing run=true until executing=true on station " + str(station))
+        time.sleep(sleep_time)
 
 
 if __name__ == '__main__':
