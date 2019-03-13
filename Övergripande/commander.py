67e68ee5f475afe5816a72843ccfa44441611798
christka1 <44722191+christka1@users.noreply.github.com>
2019-03-13 16:19:56 +0100

    Added more comments. Abstracted some methods. Did more generic.

--- Övergripande/commander.py
+++ Övergripande/commander.py
@@ -14,54 +14,65 @@ import time
 from commander_msgs.msg import Command
 from commander_msgs.msg import State
 
-from functools import partial   #Used for parsing arguments to callback function
+from functools import partial   #Used for parsing additional arguments to callback function
 
 #Defining pubs and subs
 pubs = []
 subs = []
 
 #Defining products, a station array and work order array
-product_a = "product_a"    #Assuming 2 different products
-product_b = "product_b"
-product_in_station = []                                                                             #Is this necessary?
+product_a = 'product_a'    #Assuming 2 different products
+product_b = 'product_b'
 work_order = []      #First-in-first-out order, gets popped when going to the first station
 
 #Defining message arrays
 cmd_msgs = []
 state_msgs = []
 
+#Defining the different states allowed
+init = 'init'
+executing = 'executing'
+finished = 'finished'
+
+#Defining the node
 node = None
 
+#Number of stations, hardcoded/predefined
+noOfStations = 3
+
+#Sleep time where sleep is implemented
 sleep_time = 1
 
 def main(args=None):
+    input("Press Enter to continue...")
+
     global node
 
-    noOfStations = 3
-    node = init(noOfStations)
+    node = initialize(noOfStations)     #Initializes node, creates pubs and subs and initializes the message arrays
 
-    create_work_order()     #Puts products in work_order in a predefined pattern
+    create_work_order()     #Puts products in work_order in a hardcoded/predefined pattern
 
-    while not(product_in_station[0] == None and product_in_station[1] == None and product_in_station[2] == None) or len(work_order) != 0:
-        #Skulle kunna ha en while(True) med en loop inuti som loopar igenom alla states och sätter en variabel därefter. Efter den innersta loopen en if-sats som breaker den yttersta.
+    all_stations_empty = is_all_stations_empty()
 
-        for station in range(noOfStations-1, -1, -1):
+    while not all_stations_empty or len(work_order) != 0:       #While there is something in at least one of the stations or if the work order is not empty
+
+        for station in range(noOfStations-1, -1, -1):           #From last station index down to 0
 
             print("Checking station " + str(station))
-            time.sleep(sleep_time)
             rclpy.spin_once(node)
+            time.sleep(sleep_time)
 
-            if product_in_station[station] != None:
+            if cmd_msgs[station].product_name != '':            #If station is not empty
 
                 print("Checking if station " + str(station) + " is done")
                 time.sleep(sleep_time)
 
-                if state_msgs[station].finished:
+                if check_state(station) == finished:
                     if station == noOfStations - 1:     #Last station => no station to send forward to
                         station_done(station)
 
-                    elif state_msgs[station+1].init:    #Not last station => check forward
-                        product_in_station[station+1] = product_in_station[station]
+                    elif check_state(station+1) == init:        #Not last station => check forward
+                        cmd_msgs[station+1].product_name = cmd_msgs[station].product_name
                         station_done(station)           #Should a station be done before the next station has started on the product?
                         send_command(station+1)
 
@@ -72,33 +83,36 @@ def main(args=None):
                     print("Station " + str(station) + " is not done")
 
             else:
-                if station == 0 and len(work_order) != 0:
-                    if not state_msgs[station].init:
+                if station == 0 and len(work_order) != 0:       #If nothing in the first station but the work_order is not empty
+                    if check_state(station) != init:
 
                         print("Station " + str(station) + " is not ready for another work order")
 
                     else:
                         print("Send in the next order to station " + str(station))
-                        product_in_station[station] = work_order.pop(0)
+                        cmd_msgs[station].product_name = work_order.pop(0)
                         send_command(station)
                 else:
                     print("Nothing to do")
                     time.sleep(sleep_time)
 
-    print(product_in_station)
+        all_stations_empty = is_all_stations_empty()
+
     print(work_order)
 
     node.destroy_node()
     rclpy.shutdown()
 
-def init(j):
+def state_callback(station, state_msg):
+    state_msgs[station] = state_msg
+
+#Initializes node, creates pubs and subs and initializes the message arrays
+def initialize(noOfStations):
     rclpy.init()
     node = rclpy.create_node('command_node')
 
-    for i in range(j):
+    for i in range(noOfStations):
         pubs.append(node.create_publisher(Command, 'cmd' + str(i)))
-
-        product_in_station.append(None)
 
         cmd_msgs.append(Command())
         state_msgs.append(State())
@@ -106,37 +120,57 @@ def init(j):
         subs.append(node.create_subscription(State, 'state' + str(i), partial(state_callback, i)))
     return node
 
-def state_callback(station, state_msg):
-    state_msgs[station] = state_msg
-
+#Puts products in work_order in a hardcoded/predefined pattern
 def create_work_order():
     work_order.append(product_a)
     #work_order.append(product_b)
     #work_order.append(product_a)
 
+#Returns True if all stations are empty, False otherwise
+def is_all_stations_empty():
+    rclpy.spin_once(node)
+    for cmd_msg in cmd_msgs:
+        if cmd_msg.product_name != '':
+            return False
+
+    return True
+
+#Checks and returns the state of the station. Throws exception if state is not recognized
+def check_state(station):
+    state = state_msgs[station].state
+    if state == init or state == executing or state == finished:
+        return state
+    else:
+        raise Exception("Unknown state on station " + str(station) + ": " + state)
+
+#Sets a station to init with handshake
 def station_done(station):
-    product_in_station[station] = None
+    cmd_msgs[station].product_name = ''
     cmd_msgs[station].run = False
-    while not state_msgs[station].init:
+
+    while check_state(station) != init:
         pubs[station].publish(cmd_msgs[station])
         rclpy.spin_once(node)
-        print("Pubing run=false until init=true on station " + str(station))
+        print("Pubing run=false until state=init on station " + str(station))
         time.sleep(sleep_time)
 
+#Sends command to station and puts run to true when handshake is established
 def send_command(station):
     cmd_msgs[station].command = "Assemble in station " + str(station)
     cmd_msgs[station].run = False
-    cmd_msgs[station].product_name = product_in_station[station]
-    while state_msgs[station].cmd != cmd_msgs[station].command:
+
+    while state_msgs[station].cmd != cmd_msgs[station].command:         #Waiting for handshake for the command
         pubs[station].publish(cmd_msgs[station])
         print("Waiting on station " + str(station) + " with the message " + cmd_msgs[station].command)
         rclpy.spin_once(node)
         time.sleep(sleep_time)
+
     cmd_msgs[station].run = True
-    while not state_msgs[station].executing:
+    
+    while check_state(station) != executing:
         pubs[station].publish(cmd_msgs[station])
         rclpy.spin_once(node)
-        print("Pubing run=true until executing=true on station " + str(station))
+        print("Pubing run=true until state=executing on station " + str(station))
         time.sleep(sleep_time)
 
 
