c1b8810830cee72b3c58e9ab3ecb6585b9885d09
Marcus Lindvärn <murk@live.se>
2019-04-10 17:48:18 +0200

    cleanup and bugfix

--- AGV/agv_trans_comm/src/com_agvoper.py
+++ AGV/agv_trans_comm/src/com_agvoper.py
@@ -37,59 +37,53 @@ class agv_comms():
 
     def callback_commandcenter(self, data):
         # update current and lastrecieved product to the recieved data - might skip
-        #last_product_recieved = data.product_name
-        #agv_state.product_name = last_product_recieved
         self.last_run_recieved = data.run
         
         #if a new command is recieved, update current command and refresh the view
-        print ("last_command_recieved: " + self.last_command_recieved + '\n')
-        print ("data.command: " + data.command + '\n')
-        print (self.last_command_recieved != data.command)
         if (self.last_command_recieved != data.command):
             self.last_command_recieved = data.command
             self.current_cmd = data.command
             agv_state.cmd = self.current_cmd
             self.refresh_view()
-        
         if (self.last_run_recieved == False and self.current_state == "finished"):
-            print('in reset')
             self.current_state = "init"
             agv_state.state = self.current_state
-        #always anser a publish with a publish.
+            
+        #always anser a publish with a ublish.
         self.ccpub.publish(agv_state)
 
 
     def callback_button_state(self, data):
         #agv_state = State()
         # if X is pressed, set status and send
-        #print ("xpressed: " + data.xpress + '\n')
-        #print ("bpressed: " + data.bpress + '\n')
         if (self.current_state == "init"):
             if (data.xpress == True):
-                print('sending execute')
                 self.current_state = "executing"
                 self.current_cmd = self.last_command_recieved
                 agv_state.state = self.current_state
                 agv_state.cmd = self.current_cmd
-        
+                self.ccpub.publish(agv_state)
+                self.refresh_view()
+
         # if B is pressed, set status and send
         if (self.current_state == "executing"):
             if (data.bpress == True):
-                print('sending finished')
                 self.current_state = "finished"
                 agv_state.state = self.current_state
                 self.current_cmd = ""
                 agv_state.cmd = self.current_cmd
-        #Publish with new changes
-        self.ccpub.publish(agv_state)  # kanske flytta in
+                self.ccpub.publish(agv_state) 
+                self.refresh_view()
 
     def refresh_view(self):
-        print('Last command: ' + self.last_command_recieved)
+         print('\rCurrent command: ' + self.current_cmd )
+         print('\rCurrent State: ' + self.current_state)
+         print('\rRun: ' + str(self.last_run_recieved))
       
 
 if __name__ == '__main__':
     try:
         agv_comms()
-        rospy.init_node('AGVClassCom')
     except rospy.ROSInterruptException:
         pass
+
