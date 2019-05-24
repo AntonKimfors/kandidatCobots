43276de88ca812159434cc96a6951cf903497ff2
br1nks <brinks@student.chalmers.se>
2019-05-24 11:10:52 +0200

    dir

--- commander_py/generatelog.py
+++ commander_py/generatelog.py
@@ -6,6 +6,10 @@ dirname = os.path.dirname(__file__)
 relativedirname = os.path.join(dirname, '/logs/')
 currentDate = datetime.date.today()
 
+log_dir = Path("{}{}".format(dirname, relativedirname))
+if not log_dir.exists():
+    os.makedirs(log_dir)
+
 my_file = Path("{}{}{}{}".format(dirname, relativedirname, currentDate, '.txt'))
 print(my_file)
 
@@ -13,3 +17,6 @@ if not my_file.exists():
     print('hejhej')
     logfile = open(my_file, 'w+')
 
+
+def write_to_log():
+    
